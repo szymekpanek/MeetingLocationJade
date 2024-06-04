1bb23d209ae5e921b1a23d1bea6ae54eb6d5ebb3
Dawid <dawid.kimono@gmail.com>
2024-06-04 21:11:55 +0200

    Fixed issue with counting the distance to point P9

--- src/CityMap.java
+++ src/CityMap.java
@@ -11,14 +11,14 @@ public class CityMap {
         cityMap = new HashMap<>();
 
         // Inicjalizacja mapy miasta
-        cityMap.put("P1", Map.of("P2", 5, "P3", 10));
+        cityMap.put("P1", Map.of("P2", 5, "P3", 10,"P9",1));
         cityMap.put("P2", Map.of("P1", 5, "P4", 2));
         cityMap.put("P3", Map.of("P1", 10, "P4", 1));
         cityMap.put("P4", Map.of("P2", 2, "P3", 1, "P5", 3));
         cityMap.put("P5", Map.of("P4", 3, "P6", 6));
         cityMap.put("P6", Map.of("P5", 6, "P7", 2));
         cityMap.put("P7", Map.of("P6", 2, "P8", 4));
-        cityMap.put("P8", Map.of("P7", 4));
+        cityMap.put("P8", Map.of("P7", 4, "P9",4));
         cityMap.put("P9", Map.of("P8", 4,"P1",1));
     }
 
